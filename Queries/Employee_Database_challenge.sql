3a671bdbfba69dfb08a872d836ee1ded34f121d2
Dhivya Padmanaban <83181834+dhivyapadmanaban@users.noreply.github.com>
2021-06-12 16:06:59 -0700

    Deliverable 1 & 2 queries updated

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,8 +1,10 @@
+--------------************ DELIVERABLE 1 QUERIES *************--------------------
+
 SELECT emp_no,first_name,last_name FROM employees;
 
 SELECT titles,from_date,to_date FROM titles;
 
---Creating retirement titles table 
+--QUERY 1 : Creating retirement titles table 
 SELECT e.emp_no,
 	e.first_name,
 	e.last_name,
@@ -18,7 +20,7 @@ ORDER BY e.emp_no;
 
 SELECT * FROM retirement_titles;
 
--- Use Dictinct with Orderby to remove duplicate rows
+--QUERY 2 :  Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
 last_name,
@@ -27,12 +29,45 @@ INTO retirement_unique_titles
 FROM retirement_titles
 ORDER BY emp_no ASC, to_date DESC;
 
+SELECT * FROM retirement_unique_titles;
 
---retrieve the number of employees by their most recent job title
+-- QUERY 3 : retrieve the number of employees by their most recent job title
 SELECT titles, count(*)
 INTO retiring_titles
 FROM retirement_unique_titles
 GROUP BY titles
+ORDER BY count(*) DESC;
 
 
-ORDER BY count(*) DESC
+SELECT * FROM retiring_titles;
+
+
+------------------***************** DELIVERABLE 2 ***************---------------------
+
+SELECT emp_no, first_name, last_name, birth_date from employees;
+
+SELECT from_date,to_date from dept_emp ORDER BY to_date desc;
+
+SELECT titles from titles;
+
+
+-- Query 1 : Create mentorship eligibility table
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.titles
+INTO mentorship_eligibilty
+FROM employees e
+INNER JOIN dept_emp de
+ON e.emp_no = de.emp_no
+INNER JOIN titles t
+ON e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01'
+AND e.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY e.emp_no;
+
+--Querying from new table
+SELECT * FROM mentorship_eligibilty;
